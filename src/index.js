85d69656819670739a9d7a30ca15c86f08e3a006
kimiaki2510 <153046sk@gmail.com>
2021-05-30 20:08:19 +0900

    memoAppFinally

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import MemoPage from './memo/MemoPage';
+// import MultiForm from './MultiForm'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const st = {
+  textAlign: 'left',
+  padding: '10px'
+}
 ReactDOM.render(
-  <React.StrictMode>
+  <div style={st}>
+    {/* <MultiForm /> */}
     <App />
-  </React.StrictMode>,
+  </div>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
